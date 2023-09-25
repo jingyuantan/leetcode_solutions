007b251aa6a25f60d583f92363a7923f3e28d5b4
jingyuantan <jingyuantan1028@gmail.com>
2023-09-25 22:48:58 +0100

    longest_common_prefix (string - Easy)

---
+++ strings/longest_common_prefix.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        answer = ""
+
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return answer
+            else:
+                answer += first[i]
+
+        return answer
+
+    print(longestCommonPrefix(0, ["dogs","dog","dogs"]))
